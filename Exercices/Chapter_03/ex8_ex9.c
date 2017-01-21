f7282570bfb0498f8d55f956815044d646456c5a
cosmikcodes <cosmikcodes@gmail.com>
2017-01-21 16:06:31 -0200

    Update ex8_ex9.c

--- Exercices/Chapter_03/ex8_ex9.c
+++ Exercices/Chapter_03/ex8_ex9.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 
 int main(void){
+	
 	int i, j, next_multiple;
 
 	i = 365; j = 7;
 	next_multiple = i + j - i % j;
-	printf( "Next largest even multiple for the values i = %i and j = %i is %i\n",
+	printf ("Next largest even multiple for the values i = %i and j = %i is %i\n",
 		i, j, next_multiple);
 
 	i = 12258; j = 23;
 	next_multiple = i + j - i % j;
-	printf( "Next largest even multiple for the values i = %i and j = %i is %i\n",
+	printf ("Next largest even multiple for the values i = %i and j = %i is %i\n",
 		i, j, next_multiple);
 
 	i = 996; j = 4;
 	next_multiple = i + j - i % j;
-	printf( "Next largest even multiple for the values i = %i and j = %i is %i\n",
+	printf ("Next largest even multiple for the values i = %i and j = %i is %i\n",
 		i, j, next_multiple);
 
 
