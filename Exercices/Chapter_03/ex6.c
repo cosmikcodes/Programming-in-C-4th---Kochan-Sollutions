cb6af3687c4c16fc372cab49f911d4ca7c31d620
cosmikcodes <cosmikcodes@gmail.com>
2017-01-21 16:04:29 -0200

    Update ex6.c

--- Exercices/Chapter_03/ex6.c
+++ Exercices/Chapter_03/ex6.c
@@ -1,12 +1,12 @@
 //Calculate the polynomial 3x³ - 5x² + 6 using x = 2.55
 #include <stdio.h>
 
-int main(void){
+int main (void){
 
 	float x = 2.55;
 	float pol = 3*x*x*x - 5*x*x + 6;
 
-	printf( "For x = %g\nThe polynomial 3x³ - 5x² + 6 = %g\n", x, pol );
+	printf ("For x = %g\nThe polynomial 3x³ - 5x² + 6 = %g\n", x, pol);
 
 	return 0;
 }
