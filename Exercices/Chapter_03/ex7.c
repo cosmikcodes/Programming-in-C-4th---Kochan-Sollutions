a5a26458f8a19e419954aef1ff9060cf6de74d43
cosmikcodes <cosmikcodes@gmail.com>
2017-01-21 15:46:04 -0200

    Update ex7.c

--- Exercices/Chapter_03/ex7.c
+++ Exercices/Chapter_03/ex7.c
@@ -5,7 +5,7 @@
 int main(void){
 	float expression = (3.31e-8 * 2.01e-7) / (7.16e-6 + 2.01e-8);
 
-	printf("(3.31 x 10⁻⁸ x 2.01 x 10⁻⁷) / (7.16 x 10⁻⁶ + 2.01 x 10⁻⁸) = %e", expression);
+	printf("(3.31 x 10⁻⁸ x 2.01 x 10⁻⁷) / (7.16 x 10⁻⁶ + 2.01 x 10⁻⁸) = %e\n", expression);
 
 	return 0;
 }
