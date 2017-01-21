df014cd804a13165097af158b2db82df53b0304d
cosmikcodes <cosmikcodes@gmail.com>
2017-01-21 16:08:11 -0200

    Update ex7.c

--- Exercices/Chapter_03/ex7.c
+++ Exercices/Chapter_03/ex7.c
@@ -2,8 +2,8 @@
 //Displays the results using exponential format.
 #include <stdio.h>
 
-int main (void){
-	
+int main (void)
+{	
 	float expression = (3.31e-8 * 2.01e-7) / (7.16e-6 + 2.01e-8);
 
 	printf ("(3.31 x 10⁻⁸ x 2.01 x 10⁻⁷) / (7.16 x 10⁻⁶ + 2.01 x 10⁻⁸) = %e\n", expression);
