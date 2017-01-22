bdfd41d3aef562ce50197045da83bf1c213103d3
cosmikcodes <cosmikcodes@gmail.com>
2017-01-22 02:33:21 -0200

    Create program5.4.c

---
+++ Exercices/Chapter_05/exercice_01/program5.4.c
@@ -0,0 +1,21 @@
+//A program to determine if a number is even or odd.
+
+#include <stdio.h>
+
+int main (void)
+{
+    int number, remainder;
+
+    printf ("Enter the number to test: ");
+    scanf  ("%i", &number);
+
+    remainder = number % 2;
+
+    if ( remainder == 0 )
+        printf ("\nThe number is even!\n");
+
+    else
+        printf ("\nThe number is odd!\n");
+
+    return 0;
+}
