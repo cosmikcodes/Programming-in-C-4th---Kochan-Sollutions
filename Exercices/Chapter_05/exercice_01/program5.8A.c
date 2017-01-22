6b710940b1cb6126df1f865e8871d5aa1fd69553
cosmikcodes <cosmikcodes@gmail.com>
2017-01-22 18:09:13 -0200

    Create program5.8A.c

---
+++ Exercices/Chapter_05/exercice_01/program5.8A.c
@@ -0,0 +1,31 @@
+/*  Program to evaluate simple expressions of the form:
+                                                       number operator number
+*/
+
+#include <stdio.h>
+
+int main (void)
+{
+    float value1, value2;
+    char operator;
+
+    printf ("Enter the expression: ");
+    scanf  ("%f %c %f", &value1, &operator, &value2);
+
+    if ( operator == '+' )
+        printf ("%.2f %c %.2f = %.2f\n", value1, operator, value2, value1 + value2);
+    else if ( operator == '-' )
+        printf ("%.2f %c %.2f = %.2f\n", value1, operator, value2, value1 - value2);
+    else if ( operator == '*' )
+        printf ("%.2f %c %.2f = %.2f\n", value1, operator, value2, value1 * value2);
+    else if ( operator == '/' ) {
+        if ( value2 == 0 )
+            printf ( "Division by zero!\n");
+        else
+        printf ("%.2f %c %.2f = %.2f\n", value1, operator, value2, value1 / value2);
+    }
+    else
+        printf ("Invalid operator.\n");
+
+    return 0;
+}
